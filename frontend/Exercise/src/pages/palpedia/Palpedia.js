d31dd38cf0f03a815e5a80c6a7ea7b6a1a9fad75
Terris <27739101+Terristwj@users.noreply.github.com>
2025-01-03 12:46:16 +0800

    Day-1: Ex-2 & Ex-3- useState & useEffectt

--- frontend/Exercise/src/pages/palpedia/Palpedia.js
+++ frontend/Exercise/src/pages/palpedia/Palpedia.js
@@ -8,17 +8,26 @@ import Info from "./info/Info";
 // API connections
 import { fetchPals, fetchElements } from "../../api";
 
+/////////////////////////////
+// *Ex-2: State Management //
+/////////////////////////////
+
 function Palpedia({ backgroundView }) {
-    // (1) Used at Sidebar Component
+    // TODO: Observe the variables 'activePal' and 'setActivePal'
+    // ? Used at Sidebar Component
     const [activePal, setActivePal] = useState(null);
 
-    // (2) Used at Info Component
+    //////////////////////////////////////
+    // *Ex-3: Handling API Data - START //
+    //////////////////////////////////////
+    // ? Used at Info Component
     const [palsData, setPalsData] = useState([]);
     const [elementsData, setElementsData] = useState([]);
 
-    // Fetch data from API
-    // - Note: The 'API' will be locally inside this FE project
-    // - Day-2 will be about creating your own APIs in the BE project
+    // TODO: Observe this useEffect code snippet
+    // ? Fetch data from API
+    // ? - Note: The 'API' will be locally inside this FE project
+    // ? - Day-2 will be about creating your own APIs in the BE project
     useEffect(() => {
         fetchPals().then((data) => {
             setPalsData(data.pals);
@@ -27,18 +36,41 @@ function Palpedia({ backgroundView }) {
             setElementsData(data);
         });
     }, []);
+    ////////////////////////////////////
+    // *Ex-3: Handling API Data - END //
+    ////////////////////////////////////
 
+    // TODO: Modify this code snippet - START
     return (
         <div id="content">
+            {/* (1) Observe the variables 'activePal' and 'setActivePal' */}
+            {/* DO NOT MODIFY - START */}
             <Sidebar
                 pals={palsData}
                 activePal={activePal}
                 setActivePal={setActivePal}
                 backgroundView={backgroundView}
             />
-            <Info pal={activePal} elements={elementsData} />
+            {/* DO NOT MODIFY - END */}
+
+            {/* (2) Something is wrong in this line below */}
+            <Info pal={null} elements={elementsData} />
         </div>
     );
+    // TODO: Modify this code snippet - END
+
+    // ! Solution
+    // return (
+    //     <div id="content">
+    //         <Sidebar
+    //             pals={palsData}
+    //             activePal={activePal}
+    //             setActivePal={setActivePal}
+    //             backgroundView={backgroundView}
+    //         />
+    //         <Info pal={activePal} elements={elementsData} />
+    //     </div>
+    // );
 }
 
 export default Palpedia;
