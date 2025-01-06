5bb3b5fada59983beaa8818375fa5b1c9a99133c
caslawter <caslawter@gmail.com>
2025-01-06 14:47:19 +0800

    Updated exercise

--- backend/Exercise/controller/element.controller.js
+++ backend/Exercise/controller/element.controller.js
@@ -4,3 +4,9 @@ export const getElement = async (req, res) => {
   let results = await elementModel.getElement();
   return res.status(200).json(results);
 };
+
+export const getElementById = async (req, res) => {
+    let id = req.params.id;
+    let results = await elementModel.getElementById(id);
+    return res.status(200).json(results);
+};
